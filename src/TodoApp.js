2f393047d156a07677046908b0010c6f6262a619
sunchang1996 <1029841435@qq.com>
2017-05-24 11:34:49 +0800

    模拟数据

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import TodoHead from './TodoHead';
+import TodoItem from  './TodoItem';
 export  default  class TodoApp extends React.Component {
+    constructor(props){
+        super(props); // 父类的构造函数
+        this.state={todos:[
+            {id:Date.now(),title:'今天学习react',completed:false},//初始化默认状态
+            {id:Date.now(),title:'今天学习vue',completed:false}
+        ]}
+
+    }
     render() {
+        let main =(
+            <ul className="list-group">
+                {
+                    this.state.todos.map((todo,index)=><TodoItem todo={todo} key={index}/>)
+                }
+
+            </ul>
+        )
         return (
-            <div className="container">
+            <div className="container" style={{marginTop:30}}>
                 <div className="panel-default">
                     <div className="panel-heading">
                         <TodoHead/>
                     </div>
                     <div className="panel-body">
-
+                        {main}
                     </div>
                     <div className="panel-footer">
 
