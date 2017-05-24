21f54766d2785e2f81ee4dd335f8a1be1650dd2b
sunchang1996 <1029841435@qq.com>
2017-05-24 12:44:36 +0800

    实现删除功能

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -5,23 +5,39 @@ export  default  class TodoApp extends React.Component {
     constructor(props){
         super(props); // 父类的构造函数
         this.state={todos:[
-            {id:Date.now(),title:'今天学习react',completed:false},//初始化默认状态
-            {id:Date.now(),title:'今天学习vue',completed:false}
+            {id:Math.random(),title:'今天学习react',completed:false},//初始化默认状态
+            {id:Math.random(),title:'今天学习vue',completed:true}
         ]}
 
     }
+    toggle=(id)=>{
+        let todos = this.state.todos;
+        todos.map(todo=>{
+            if(todo.id === id){
+                todo.completed = !todo.completed;
+            }
+            return todo;
+        });
+        this.setState({todos});
+    };
     addTodo=(todo)=>{
         todo = Object.assign(todo,{id:Date.now(),completed:false});
         // todo.id= Date.now();
         let todos = this.state.todos;
         todos.push(todo);
         this.setState({todo})
+    };
+    remove= id=>{
+        let todos = this.state.todos;
+        let index = todos.findIndex(todo=>todo.id===id); // findIndex:查找符合条件的索引 找到返回当前的索引 没有找到返回-1
+        todos.splice(index,1);
+        this.setState(todos);
     }
     render() {
         let main =(
             <ul className="list-group">
                 {
-                    this.state.todos.map((todo,index)=><TodoItem todo={todo} key={index}/>)
+                    this.state.todos.map((todo,index)=><TodoItem toggle={this.toggle} todo={todo} key={index} remove={this.remove}/>)
                 }
              </ul>
         )
